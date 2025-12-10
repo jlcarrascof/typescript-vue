93bdf626fc2b0328fef89442119f6d16f9d7f7ee
Javier Martinez <entrenamientospuntonet@gmail.com>
2025-12-10 19:04:06 -0400

    Add additional logging for processError function in test cases

--- checkpoint-01/validator-ts/src/test.js
+++ checkpoint-01/validator-ts/src/test.js
@@ -3,3 +3,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var validator_1 = require("./validator");
 console.log(validator_1.validator.validateString("Hello")); // true
 console.log(validator_1.validator.validateString(123)); // false
+try {
+    var resultado = validator_1.validator.processError("vue");
+    console.log(resultado);
+    console.log(validator_1.validator.processError(true));
+}
+catch (error) {
+    console.error('Error captured:', error.message);
+}
+console.log(validator_1.validator.detectType("test")); // "string"
+console.log(validator_1.validator.detectType(42)); // "number"
+console.log(validator_1.validator.detectType(true)); // "another"
--- checkpoint-01/validator-ts/src/test.ts
+++ checkpoint-01/validator-ts/src/test.ts
@@ -7,6 +7,7 @@ console.log(validator.validateString(123)); // false
 try {
     const resultado = validator.processError("vue");
     console.log(resultado);
+    console.log(validator.processError(true));
 } catch (error) {
     console.error('Error captured:', (error as Error).message);
 }
